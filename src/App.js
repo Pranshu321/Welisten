4cc5bf5cebf960b3deb53133bf3783b854cbd800
ritikmasand <ritikmasand10@gmail.com>
2022-11-19 11:12:24 +0530

    blog page added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import SignUp from "./components/SignUp";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Landing from "./Landing";
 import Dashboard from "./components/Dashboard";
+// import Landing from "./Landing";
+import Homee from "./components/BlogPage/pages/Homee";
+import Blog from "./components/BlogPage/pages/Blog";
+import Error from "./components/BlogPage/pages/Error";
 
 export default function App() {
   return (
@@ -19,14 +23,19 @@ export default function App() {
       <Router>
         <Switch>
           <Route exact path="/">
+            {/* <Route path="/" element={<Landing />} /> */}
             <Landing />
+            {/* <Homee /> */}
           </Route>
-          <Route path="/dashboard">
+          <Route exact path="/dashboard">
+            <Route exact path="/Homee" element = {<Homee/>} />
+            <Route exact path="/blog" element={<Blog />} />
+            <Route exact path="/blog/:id" element={<Blog />} />
+            <Route exact path="/*" element={<Error />} />
             <Dashboard />
           </Route>
         </Switch>
       </Router>
-
     </div>
   );
 }
