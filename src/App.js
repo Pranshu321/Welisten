d901c06e814124c6936b30ab33f238e03f09e98c
Pranshu321 <pranshujain0111@gmail.com>
2022-11-19 12:04:35 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
 import React from "react";
-import AboutUs from "./components/AboutUs";
-import DailyPrices from "./components/DailyPrices";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Newsletter from "./components/Newsletter";
-import Roadmap from "./components/Roadmap";
-import ScrollToTop from "./components/ScrollToTop";
-import Services from "./components/Services";
-import SignUp from "./components/SignUp";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Landing from "./Landing";
 import Dashboard from "./components/Dashboard";
@@ -26,7 +16,6 @@ export default function App() {
           </Route>
         </Switch>
       </Router>
-
     </div>
   );
 }
